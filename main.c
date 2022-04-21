acc86ae00722e731ed415aa85962a44d04e2bfe1
Mxtqnt <anacarolinass.cc@gmail.com>
2022-04-21 11:15:38 -0300

    Registradores

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+    //1 MBR – Memory Buffer Register
+    //2 MAR – Memory Address Register
+    //6 IMM – Immediate
+    //7 PC – Program Counter
+    unsigned int mbr, mar, imm, pc;
+
+    //9 r0 a r7
+    unsigned int reg[8];
+
+    //3 IR – Instruction Register
+    //4 RO0 – Register Operand 0
+    //5 RO1 – Register Operand 1
+    //8 E, L e G
+    unsigned char ir, ro0, ro1, e, l, g;
+
+
+    printf("Hello World\n");
+
+
+
+
+
+
+    return 0;
+}
